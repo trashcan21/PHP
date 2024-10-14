df2e803352f930fd88cf0ba745e4c1f55f0e8f73
trashcan21 <157221221+trashcan21@users.noreply.github.com>
2024-10-14 18:52:05 +0500

    Add files via upload

---
+++ dataget.php
@@ -0,0 +1,47 @@
+<?php
+
+$sname = $_GET  ['mov_name'];
+$sdate = $_GET ['mov_date'];
+$sbusiness = $_GET ['business'];
+$sactor = $_GET ['act_name'];
+$sactress = $_GET ['actress_name'];
+$snetflix = $_GET ['avail_net'];
+$samazon = $_GET ['avail_prime'];
+$sgenre = $_GET ['genre'];
+$srating = $_GET ['rating'];
+
+echo "$sname";
+
+echo "<br>";
+
+echo "$sdate";
+
+echo "<br>";
+
+echo "$sbusiness";
+
+echo "<br>";
+
+echo "$sactor";
+
+echo "<br>";
+
+echo "$sactress";
+ 
+echo "<br>";
+
+echo "$snetflix";
+
+echo "<br>";
+
+echo "$samazon";
+
+echo "<br>";
+
+echo "$sgenre";
+
+echo "<br>";
+
+echo "$srating";
+
+?>
---
+++ salman3.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+
+
+<?php
+
+$movie_name="venom";
+
+$movie_poster="http://wallpapers.com/images/featured/venom-pictures-fy7adc0g6sv3v73n.jpg";
+
+
+$release_year=2018;
+
+$available_on_netflix="no";
+
+$lead_actor="Tom Hardy";
+
+$lead_actress="michelle villiams";
+
+
+echo "<h1>$movie_name</h1>";
+
+echo "<br>";
+
+echo "$movie_poster";
+
+echo "<br>";
+
+echo "<h3>$release_year</h3>";
+
+echo "<br>";
+echo "<h3>$available_on_netflix</h3>";
+
+echo "<br>";
+echo "<h3>$lead_actor</h3>";
+
+echo "<br>";
+echo "$lead_actress";
+
